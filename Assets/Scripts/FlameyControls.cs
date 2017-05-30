cc671519f45c7f84e99026949d61b1c603e0c048
Jonathan Hirz <jonathanhirz@gmail.com>
2017-05-29 22:05:39 -0700

    material changes, break out tree model

--- Assets/Scripts/FlameyControls.cs
+++ Assets/Scripts/FlameyControls.cs
@@ -11,6 +11,7 @@ public class FlameyControls : MonoBehaviour {
 	public float maxHeadRotation = 30.0f;
 	public float jumpSpeed = 15.0f;
 	public float gravity = 30.0f;
+	public float pushPower = 2.0f;
 
 	Rigidbody rb2d;
 	CharacterController controller;
@@ -51,4 +52,16 @@ public class FlameyControls : MonoBehaviour {
 		controller.Move(transform.TransformDirection(input * speed * Time.deltaTime + yVelocity * Vector3.up * Time.deltaTime));
 
 	}
+
+	// void OnControllerColliderHit(ControllerColliderHit hit) {
+	// 	Rigidbody body = hit.collider.attachedRigidbody;
+	// 	if (body == null || body.isKinematic)
+	// 					return;
+
+	// 	if (hit.moveDirection.y < -0.3F)
+	// 					return;
+
+	// 	Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+	// 	body.velocity = pushDir * pushPower;
+	// }
 }
